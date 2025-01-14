0c828686a69db6f53bdd4f89f96631ef6fea2f3c
Chika Chijioke <8pluscreative@gmail.com>
2025-01-14 03:17:44 +0100

    Order Service

---
+++ order-service/src/main/java/com/cjchika/microservices/order_service/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.cjchika.microservices.order_service.service;
+
+import com.cjchika.microservices.order_service.dto.OrderRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    public void placeOrder(OrderRequest orderRequest){
+
+    }
+}
