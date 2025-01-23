5388da59163dc3e6326f226d28ce2238e0393ba9
Chika Chijioke <8pluscreative@gmail.com>
2025-01-23 08:48:28 +0100

    Circuit Breaker - Resilience4j

--- order-service/src/main/java/com/cjchika/microservices/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/com/cjchika/microservices/order_service/config/RestClientConfig.java
@@ -2,11 +2,17 @@ package com.cjchika.microservices.order_service.config;
 
 import com.cjchika.microservices.order_service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+//import org.springframework.boot.web.client.ClientHttpRequestFactories;
+//import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -23,4 +29,11 @@ public class RestClientConfig {
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
+
+//    private ClientHttpRequestFactory getClientRequestFactory() {
+//        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.defaults()
+//                .withConnectTimeout(Duration.ofSeconds(3))
+//                .withReadTimeout(Duration.ofSeconds(3));
+//        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
+//    }
 }
