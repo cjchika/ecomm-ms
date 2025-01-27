0fe6d8f00550802cbbcacb570af0c15fec3deef5
Chika Chijioke <8pluscreative@gmail.com>
2025-01-27 17:41:26 +0100

    Monitoring - Grafana Stack

--- order-service/src/main/java/com/cjchika/microservices/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/com/cjchika/microservices/order_service/config/RestClientConfig.java
@@ -1,13 +1,19 @@
 package com.cjchika.microservices.order_service.config;
 
 import com.cjchika.microservices.order_service.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+//import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+//import org.springframework.boot.web.client.ClientHttpRequestFactories;
 //import org.springframework.boot.web.client.ClientHttpRequestFactories;
 //import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.client.ClientHttpRequestFactory;
+//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -15,23 +21,34 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
 
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
 
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+        return factory;
+    }
+
 //    private ClientHttpRequestFactory getClientRequestFactory() {
-//        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.defaults()
+//        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
 //                .withConnectTimeout(Duration.ofSeconds(3))
 //                .withReadTimeout(Duration.ofSeconds(3));
 //        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
